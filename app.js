098c007e1a7ba0e41c035962c48aaa912a0c2e4b
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-06-12 22:56:46 +0500

    remove git modules from the github

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express")
 
 const app = express()
 
+app.get('/', (req,res)=>{
+ res.status(200).json({messge:"Hello from the server side"})
+})
+
 const PORT =3000
 app.listen(PORT, ()=>{
-    console.log("Server successfully started")
+    console.log(`Server successfully started at ${PORT}`)
 })
