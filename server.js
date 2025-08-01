1e88057d26815957de65a217771b7881ef02fb78
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-08-01 12:52:19 +0500

    connecting mongoose with express

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+const mongoose = require('mongoose')
 const dotenv  = require('dotenv')
 const app =  require('./app')
 
 dotenv.config({path:'./config.env'})
 
+const DB = process.env.DATABASE
+
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+
+}).then((con)=>{
+    console.log(con.connection)
+    console.log("DB CONNECTIONS")
+})
 //In this file we will add the stuff that is not related to the express and is mainly related to the server
 
 
