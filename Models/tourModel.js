9fcd73fb83f4b3091b66393ef1c29fc7840b27da
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-09-19 11:40:42 +0500

    document middleware in mongodb

--- Models/tourModel.js
+++ Models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const slugify = require('slugify')
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -9,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       minlength: [3, 'Name must be at least 3 characters'],
     },
+    slug: String,
     duration:{
     type:Number,
     required:[true,'A tour must have a duration']
@@ -75,6 +76,25 @@ tourSchema.virtual('durationWeeks').get(function(){
   return this.duration/7
 })
 
+
+//Document MIDDLEWARE : runs before .save() and .create().
+
+tourSchema.pre('save', function(next){
+  this.slug = slugify(this.name, {lower:true})
+  next()
+})
+
+
+// tourSchema.pre('save', function(next){
+// console.log('Will save document...')
+// next() 
+// })
+
+
+// tourSchema.post('save', function(doc,next){
+//   console.log(doc)
+//   next()
+// })
 
 tourSchema.index({ name: 1 }, { unique: true });
 
