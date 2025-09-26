23fe19357aebd8017e9b6e605923ec6052cef635
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-09-26 14:50:27 +0500

    Added the global error

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const morgan = require('morgan')
 const tourRouter = require("./routes/tourRoutes")
 const userRouter = require('./routes/userRoutes')
+const { status } = require("express/lib/response")
 
 
 
@@ -28,12 +29,27 @@ app.use((req,res,next)=>{
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/user', userRouter)
 
-
+//This is the middleware for the unhandled routes
 app.all('*', (req,res,next)=>{
     res.status(404).json({
         status:'Fail',
         message:`Can't find ${req.originalUrl} on this server`
     })
 })
+
+
+//Global Error middleware
+
+app.use((err, req,res,next)=>{
+    err.statusCode = err.statusCode || 500,
+    err.status = err.status || 'error'
+
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
+    })
+})
+
+
 
 module.exports = app
